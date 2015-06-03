ccda92cb424bf1b4a6a85a3caa2015ffac40a102
goattea <robert@goattea.com>
2015-06-03 10:11:58 -0600

    Fizz Buzz Kata
    Problem Description
    
    Imagine the scene. You are eleven years old, and in the five minutes
    before the end of the lesson, your Maths teacher decides he should make
    his class more "fun" by introducing a "game". He explains that he is
    going to point at each pupil in turn and ask them to say the next number
    in sequence, starting from one. The "fun" part is that if the number is
    divisible by three, you instead say "Fizz" and if it is divisible by
    five you say "Buzz". So now your maths teacher is pointing at all of
    your classmates in turn, and they happily shout "one!", "two!", "Fizz!",
    "four!", "Buzz!"... until he very deliberately points at you, fixing you
    with a steely gaze... time stands still, your mouth dries up, your palms
    become sweatier and sweatier until you finally manage to croak "Fizz!".
    Doom is avoided, and the pointing finger moves on.
    
    So of course in order to avoid embarassment infront of your whole class,
    you have to get the full list printed out so you know what to say. Your
    class has about 33 pupils and he might go round three times before the
    bell rings for breaktime. Next maths lesson is on Thursday. Get coding!
    
    Write a program that prints the numbers from 1 to 100. But for multiples
    of three print "Fizz" instead of the number and for the multiples of
    five print "Buzz". For numbers which are multiples of both three and
    five print "FizzBuzz?".
    
    Sample output:
    
    1
    2
    Fizz
    4
    Buzz
    Fizz
    7
    8
    Fizz
    Buzz
    11
    Fizz
    13
    14
    FizzBuzz
    16
    17
    Fizz
    19
    Buzz
    ... etc up to 100
    
    Stage 2 - new requirements
    
    * A number is fizz if it is divisible by 3 or if it has a 3 in it
    * A number is buzz if it is divisible by 5 or if it has a 5 in it

---
+++ FizzBuzzKata.UnitTests/FizzBuzzTester.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.UnitTests
+{
+	[TestFixture]
+	public class FizzBuzzTester
+	{
+		readonly FizzBuzzer _translator = new FizzBuzzer();
+
+		[Test, Category("Unit")]
+		public void InstantiateTranslator()
+		{
+			//Arrange
+
+			//Act
+			//Assert
+
+			Assert.IsNotNull(_translator);
+		}
+
+		[Test, Category("Unit")]
+		public void When_AskedABoutOne_ReturnsOne()
+		{
+			//Arrange
+			//Act
+			var answer = _translator.Translate(1);
+
+			//Assert
+			Assert.AreEqual(1.ToString(), answer);
+		}
+
+		[Test, Category("Unit")]
+		public void When_Three_ReturnFizz()
+		{
+			//Arrange
+			//Act
+
+			var answer = _translator.Translate(3);
+			//Assert
+			Assert.AreEqual("Fizz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void WhenFour_DoNot_ReturnFizz()
+		{
+			//Arrange
+			//Act
+
+			var answer = _translator.Translate(4);
+			//Assert
+			Assert.AreNotEqual("Fizz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void WhenFive_Return_Buzz()
+		{
+			//Arrange
+			//Act
+
+			var answer = _translator.Translate(5);
+			//Assert
+			Assert.AreEqual("Buzz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void WhenTenReturnBuzz()
+		{
+			//Arrange
+			//Act
+
+			var answer = _translator.Translate(10);
+			//Assert
+			Assert.AreEqual("Buzz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void FifteenReturnsFizzBuzz()
+		{
+			//Arrange
+			//Act
+
+			var answer = _translator.Translate(15);
+			//Assert
+			Assert.AreEqual("FizzBuzz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void FiftyTwoReturnsBuzz()
+		{
+			var answer = _translator.Translate(52);
+
+			Assert.AreEqual("Buzz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void PrintFunList()
+		{
+			for(int i=1;i<=100;i++)
+			{
+				System.Diagnostics.Debug.WriteLine(_translator.Translate(i));
+			}
+		}
+
+		[Test, Category("Unit")]
+		public void ThirtyOneReturnsFizz()
+		{
+			var answer = _translator.Translate(31);
+
+			Assert.AreEqual("Fizz", answer);
+		}
+
+		[Test, Category("Unit")]
+		public void thirtyFiveReturnsFizzBuzz()
+		{
+			//Arrange
+			var answer = _translator.Translate(35);
+			//Act
+			//Assert
+			Assert.AreEqual("FizzBuzz", answer);
+		}
+	}
+
+
+
+	public class FizzBuzzer
+	{
+		public string Translate(int input)
+		{
+			if (IsDivisibleByThree(input) && IsDivisibleByFive(input))
+				return "FizzBuzz";
+
+			if (IsDivisibleByThree(input))
+				return "Fizz";
+
+			if (input.ToString().Contains("3") && input.ToString().Contains("5"))
+				return "FizzBuzz";
+			if (IsDivisibleByFive(input))
+				return "Buzz";
+
+			if (input.ToString().Contains("3"))
+				return "Fizz";
+
+			if (input.ToString().Contains("5"))
+				return "Buzz";
+
+			
+
+
+			return input.ToString();
+		}
+
+		private bool IsDivisibleByThree(int input)
+		{
+			return input % 3 == 0;
+		}
+
+		private bool IsDivisibleByFive(int input)
+		{
+			return input % 5 == 0;
+		}
+
+	}
+}
